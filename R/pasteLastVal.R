b6885b76db204cd91f7e4baa474ea93c5e7e7b75
Andrew Lawrence <AndrewLawrence@users.noreply.github.com>
2021-05-11 18:13:11 +0100

    codestyle/lint changes

--- R/pasteLastVal.R
+++ R/pasteLastVal.R
@@ -6,32 +6,38 @@
 #     https://github.com/fraupflaume
 #     https://github.com/rstudio/addinexamples
 
+
 #' pasteLastVal
+#'
 #' Pastes the last value as a comment.
 #'
 #' @import rstudioapi stringr
 #' @export
 pasteLastVal <- function() {
-  outputstr = utils::capture.output(
+  outputstr <- utils::capture.output(
     tryCatch(
       print(.Last.value),
       warning = "",
       error = ""
     ))
 
-  outputstr = paste(outputstr, collapse = "\n")
-  outputstr = paste("#", outputstr)
-  outputstr = gsub("\n", "\n# ", outputstr)
-  outputstr = paste(outputstr, "\n")
+  outputstr <- paste(outputstr, collapse = "\n")
+  outputstr <- paste("#", outputstr)
+  outputstr <- gsub("\n", "\n# ", outputstr)
+  outputstr <- paste(outputstr, "\n")
 
-  outputstr = stringr::str_replace_all(outputstr,stringr::regex("(\\033.*?m)"),"")
+  outputstr <- stringr::str_replace_all(outputstr,
+                                        stringr::regex("(\\033.*?m)"),
+                                        "")
 
+  rstudioapi::insertText(text = outputstr,
+                         id = rstudioapi::getSourceEditorContext()$id)
 
-  rstudioapi::insertText(text=outputstr, id = rstudioapi::getSourceEditorContext()$id)
+  ed_sel <- rstudioapi::getSourceEditorContext()$selection
+  ed_row <- ed_sel[[1]][["range"]][["end"]][1]
 
   rstudioapi::setCursorPosition(
-    rstudioapi::document_position(rstudioapi::getSourceEditorContext()$selection[[1]][["range"]][["end"]][1], 1),
-    id = rstudioapi::getSourceEditorContext()$id)
+    rstudioapi::document_position(ed_row, 1),
+    id = rstudioapi::getSourceEditorContext()$id
+  )
 }
-
-
